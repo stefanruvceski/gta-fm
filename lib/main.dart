39b1bb9b9e5fcd16f91662dbd44a427e463ff561
Stefan Ruvceski <stefanruvceski@gmail.com>
2021-03-03 23:54:40 +0100

    firt try

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+final mp3Url =
+    "http://docs.google.com/uc?export=open&id=1Bn_3HikUes4JD-Y1oH61zif3duQcPu6M";
+var dio = Dio();
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'GTA FM',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'GTA FM'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,68 +33,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String downloadingProgress;
+  final AssetsAudioPlayer _player = AssetsAudioPlayer.newPlayer();
 
-  void _incrementCounter() {
+  final Playlist playlist = Playlist(audios: [
+    Audio.network(
+      mp3Url,
+      metas: Metas(title: 'hello world'),
+    ),
+    Audio.network(
+      mp3Url,
+      metas: Metas(title: 'hello world 2'),
+    ),
+  ]);
+
+  @override
+  void initState() {
+    super.initState();
+    _player.currentPosition.listen((event) {
+      //print("_player.currentPosition: $event");
+    });
+    _player.open(
+      playlist,
+      autoStart: false,
+      showNotification: true,
+    );
+    _downloadInParallel();
+  }
+
+  void _downloadInParallel() async {
+    final tempDir = await getTemporaryDirectory();
+    final downloadPath = tempDir.path + "/downloaded.mp3";
+    print('full path $downloadPath');
+
+    await Future.delayed(Duration(seconds: 3));
+
+    await downloadFileTo(
+        dio: dio,
+        url: mp3Url,
+        savePath: downloadPath,
+        progressFunction: (received, total) {
+          if (total != -1) {
+            setState(() {
+              downloadingProgress =
+                  (received / total * 100).toStringAsFixed(0) + "%";
+            });
+          }
+        });
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      print("downloaded, switching to file type $downloadPath");
+      playlist.replaceAt(
+        0,
+        (oldAudio) {
+          return oldAudio.copyWith(
+              audioType: AudioType.file, path: downloadPath);
+        },
+        keepPlayingPositionIfCurrent: true,
+      );
+      this.downloadingProgress = null;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            if (downloadingProgress != null)
+              Text(this.downloadingProgress)
+            else
+              SizedBox(),
+            _playingButton(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _playingButton() {
+    return PlayerBuilder.isPlaying(
+      player: _player,
+      builder: (context, isPlaying) {
+        return FloatingActionButton(
+          child: isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+          onPressed: () {
+            _player.playOrPause();
+          },
+        );
+      },
+    );
+  }
+}
+
+Future downloadFileTo(
+    {Dio dio,
+    String url,
+    String savePath,
+    Function(int received, int total) progressFunction}) async {
+  try {
+    final Response response = await dio.get(
+      url,
+      onReceiveProgress: progressFunction,
+      //Received data with List<int>
+      options: Options(
+          responseType: ResponseType.bytes,
+          followRedirects: false,
+          validateStatus: (status) {
+            return status < 500;
+          }),
+    );
+    print(response.headers);
+    final File file = File(savePath);
+    var raf = file.openSync(mode: FileMode.write);
+    // response.data is List<int> type
+    raf.writeFromSync(response.data);
+    await raf.close();
+  } catch (e) {
+    print(e);
   }
 }
