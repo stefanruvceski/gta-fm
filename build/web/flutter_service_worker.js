b9a84bddb1476ff395f84403f83a858c7e891a34
Stefan Ruvceski <stefanruvceski@gmail.com>
2021-03-04 15:57:12 +0100

    build 1.2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0d992fb9f7dacb42f59a24267f4c86ff",
-"/": "0d992fb9f7dacb42f59a24267f4c86ff",
-"main.dart.js": "82eb1e1743053ac137a288e9449daa6e",
+"index.html": "d91c503c6d01bef75097e623c0a7b648",
+"/": "d91c503c6d01bef75097e623c0a7b648",
+"main.dart.js": "d505b4284dc7aa45997c36e91c2fc348",
 "manifest.json": "e1484de32465026b1e09c3c2653ccfa1",
 "version.json": "bdfb9354a7fa0973d1781ffa2543a5ad"
 };
